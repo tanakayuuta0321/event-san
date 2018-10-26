b7418e83808286dbfffb97fb1b1e1479f212e587
tanakayuuta <>
2018-10-26 22:54:18 +0900

    implements search method

--- event-san/src/app/app.module.ts
+++ event-san/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { EventProvider } from '../providers/event/event';
 
+// イベントプロパイダーはHTTP通信するため、HttpClientModuleのインポートが必要
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -15,6 +19,8 @@ import { EventProvider } from '../providers/event/event';
   ],
   imports: [
     BrowserModule,
+    // イベントプロパイダーはHTTP通信するため、HttpClientModuleのインポートが必要
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- event-san/src/providers/event/event.ts
+++ event-san/src/providers/event/event.ts
@@ -1,17 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+// HttpParams でHTTPパラメータをconnpassのAPIに指定する
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+// HttpParams の戻り値を受け取るObservable
+import { Observable } from 'rxjs/Observable';
 
-/*
-  Generated class for the EventProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class EventProvider {
 
   constructor(public http: HttpClient) {
     console.log('Hello EventProvider Provider');
+  }
+
+  search(keywords: string[]): Observable<Object> {
+
+    let params = new HttpParams()
+      .append('order', '2')
+      .append('count', '100');
+    keywords.forEach(kwd => params = params.append('keyword', `${kwd}`));
+
+    return this.http.get("/connpass/api", { params: params });
   }
 
 }
